5b7d1fbc442eaa1c548b83e2b82e64c17d4b38d8
yusukesugahara <yusukesugahara@sugawarayuusukenoMacBook-Pro.local>
2019-03-29 20:37:15 +0900

    create top_view_page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "tops#index"
+  resources :tops, only: :index
+  resources :brands
+  resources :categories
+  resources :mypages
+
+
 end
