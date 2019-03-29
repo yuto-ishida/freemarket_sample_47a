ced108a3310bb537349fffe92779c8604863abd4
yusukesugahara <yusukesugahara@sugawarayuusukenoMacBook-Pro.local>
2019-03-29 21:00:42 +0900

    Edit tops_scss toutes_rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "tops#index"
   resources :tops, only: :index
-  resources :brands
-  resources :categories
-  resources :mypages
+  resources :brands, only: :index
+  resources :categories only: :index
+  resources :mypages, only: [:new, :create, :update, :show ,:destroy]
 
 
 end
