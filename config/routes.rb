637c9bf44cd5f81a0becde1f6599bd6d4708d953
yusukesugahara <yusukesugahara@sugawarayuusukenoMacBook-Pro.local>
2019-03-31 16:21:10 +0900

    Create_tops_new_view

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "tops#index"
-  resources :tops, only: :index
+  resources :tops, only: [:index, :new]
   resources :brands, only: :index
   resources :categories, only: :index
   resources :mypages, only: [:new, :create, :update, :show ,:destroy]
