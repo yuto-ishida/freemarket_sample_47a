fddce6425863bc5aea0bff43a7f370865c2a0f01
yusukesugahara <yusukesugahara@sugawarayuusukenoMacBook-Pro.local>
2019-04-05 10:58:56 +0900

    edit_view_files_add_links

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def after_sign_in_path_for(resource)
+    mypage_path(current_user) # ログイン後に遷移するpathを設定
+  end
+
+  def after_sign_out_path_for(resource)
+    root_path # ログアウト後に遷移するpathを設定
+  end
 end
