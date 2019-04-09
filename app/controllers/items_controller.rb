99d47d077b446af73676740b2621b6ab1a527067
yusukesugahara <yusukesugahara@sugawarayuusukenoMacBook-Pro.local>
2019-04-09 20:23:51 +0900

    create_exhibition_correction2

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,15 +8,23 @@ class ItemsController < ApplicationController
   end
 
   def create
+    binding.pry
     @item = Item.new(item_params)
     if @item.save
       redirect_to root_path
     else
+      ItemImage.where(id: item_image_params ).destroy_all
       render :new
+    end
   end
 
   private
   def item_params
     params.require(:item).permit(:status_id ,{:item_image_ids => []},:category_ids, :item_size_ids, :brand_ids ,:name,:description,:condition_id,:shipping_burden_id, :shipping_style_id ,:prefecture_id,:date_of_shipment_id ,:price).merge(user_id: current_user.id,)
+  end
+  def item_image_params
+    image_ids = params.require(:item).permit(:item_image_ids => [])
+    item_image_ids = image_ids[:item_image_ids]
+    return item_image_ids
   end
 end
