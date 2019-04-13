a52ec56a2a764f9729da8e19f2b5a134cc01f150
yusukesugahara <yusukesugahara@sugawarayuusukenoMacBook-Pro.local>
2019-04-13 23:35:20 +0900

    create_delete_function

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,18 +1,18 @@
 class Item < ApplicationRecord
   belongs_to :user
-  has_many :CategoryItems
+  has_many :CategoryItems, :dependent => :destroy
   has_many :categories, through: :CategoryItems
   has_many :BrandItems
   has_many :brands, through: :BrandItems
   has_many :item_image_items
-  has_many :item_images, through: :item_image_items
+  has_many :item_images, through: :item_image_items, :dependent => :destroy
   belongs_to :condition
   belongs_to :shipping_burden
   belongs_to :shipping_style
   belongs_to :date_of_shipment
   has_many :credit
   has_many :buyer
-  has_many :item_size_items
+  has_many :item_size_items, :dependent => :destroy
   has_many :item_sizes, through: :item_size_items
   belongs_to :status
   has_many :messages, :dependent => :destroy
