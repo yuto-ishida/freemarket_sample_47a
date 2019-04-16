09170a0d0cc21b87944d20dad319e9430121ffb0
tomozou-miyake <tomoki2802@gmail.com>
2019-04-16 20:52:36 +0900

    Add sold

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -93,7 +93,7 @@ class ItemsController < ApplicationController
 
   private
   def item_params
-    params.require(:item).permit(:status_id ,:category_ids, :item_size_ids, :brand_ids ,:name,:description,:condition_id,:shipping_burden_id, :shipping_style_id ,:prefecture_id,:date_of_shipment_id ,:price,item_images_attributes: [:image]).merge(user_id: current_user.id)
+    params.require(:item).permit(:id,:status_id ,:category_ids, :item_size_ids, :brand_ids ,:name,:description,:condition_id,:shipping_burden_id, :shipping_style_id ,:prefecture_id,:date_of_shipment_id ,:price,item_images_attributes: [:image]).merge(user_id: current_user.id)
   end
 
   def set_category
